28b9601fda1433d6d77c850b4f77a4f9b0dfc08d
cozy1712 <kolawolekabiru89@gmail.com>
2023-03-15 01:28:55 +0100

    updated styling

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,37 +1,66 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Card from '../shared/Card'
+<script src="https://smtpjs.com/v3/smtp.js"></script>
+
 
 function ContactPage() {
+
+//   const sendEmail = () =>{
+
+
+//   function sendEmail(Email) {
+            
+//     Email.send({ 
+//       secureToken :"398515c4-0604-491e-8667-4d67f0220096",
+//       To : "akangbekabiru89@gmail.com",
+//       From : " ",
+//       Subject : "",
+//       Body : "",
+//     }).then(
+//       Message => alert("Message has being sent successfully"))
+    
+//   }
+// }
+////////////////onSubmit="sendEmail(); reset(); return false;"
   return (
     <Card>
-        <form >
-          <div className='input-group'>
-             <input type="text" name='name' placeholder='Name' style={{
-             height:"50px", borderRadius:"10px", border:"3px solid skyblue", fontWeight:"bold",padding:"3px"}}/>
-            </div><br />
-            <div  className='input-group'>
-              <input type="email"name='email' placeholder='Email'style={{
-              height:"50px", borderRadius:"10px", border:"3px solid skyblue", fontWeight:"bold",padding:"3px"}}></input>
-             </div><br />
-             <div  className='input-group'>
-                <textarea name="text" id="msg" cols="100" rows="10" placeholder=" Enter Your Message Here"style={{
-               borderRadius:"10px", border:"3px solid skyblue", fontWeight:"bold",padding:"3px"}}></textarea>
-             </div><br />
-             <div className='message'>
-                <button type="submit" style={{ width:"70px",
-                height:"40px", borderRadius:"10px", border:"2px solid skyblue", fontWeight:"bold",padding:"3px",backgroundColor:"darkgreen" ,justifyContent:"centre", cursor:"pointer"}} >Send</button>
-            </div>
-          
-
+        <form  autoComplete='off' className='input-contactpage'>
+      
+          <input type="text" id='name' placeholder='Name'/>
+          <input type="email" id='email' placeholder='Email' required/>
+          <input type="text" id='phone' placeholder='Phone No.' required/>
+          <textarea name="text" id="msg" rows="5" placeholder=" Enter Your Message Here"/>
+          <button type="submit" >Send</button>
+        
         </form>
         <p>
-          <Link to = '/' style={{
-            color:"skyblue", textDecoration:"none", padding:"10px", margin:"20px"
-          }}>Back to home </Link>
+          <Link to = '/' className='contact-link'>Back to home </Link>
         </p>
+        {/* <script>
+          function sendEmail() {
+            Email.send({ 
+              secureToken :"398515c4-0604-491e-8667-4d67f0220096",
+              To : "akangbekabiru89@gmail.com",
+              From : document.getElementById('email').value,
+              Subject : " New Contact From Enquiry",
+              Body : " Name: "+ getElementById('name').value
+               +"<br> Email: "+ getElementById('email').value
+               +"<br> Phone No: "+ getElementById('phone').value
+               +"<br> Message: "+ getElementById('msg').value,
+            }).then(
+              message => alert("Message has being sent successfully"))
+            
+          }
+
+
+
+        </script> */}
     </Card>
   )
 }
 
 export default ContactPage
+
+// securetooken
+// 398515c4-0604-491e-8667-4d67f0220096
